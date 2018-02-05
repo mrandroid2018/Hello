439e4a52c2e36cfa145483ce4bdcafeaf528231d
mrandroid2018 <36158282+mrandroid2018@users.noreply.github.com>
2018-02-05 21:36:07 +0900

    Add Folder

---
+++ java/com/kitesoft/hellogithub/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kitesoft.hellogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //한글 테스트 입니다.
+    }
+}
